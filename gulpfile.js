2c5b220084503e9f51546f138a605c94f89da24b
jahzlariosa <joseph.lariosa@pantheon.io>
2020-11-10 03:11:55 +0800

    v1.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -16,17 +16,14 @@ var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
 var clean = require("gulp-clean");
-
-const print = require("gulp-print").default;
-const minify = require('gulp-minify');
+var log = require("fancy-log");
+const minify = require("gulp-minify");
 const cleanCSS = require("gulp-clean-css");
 const size = require("gulp-size");
-const imagemin = require('gulp-imagemin');
+const imagemin = require("gulp-imagemin");
 
 var cfg = require("./gulpconfig.json");
 var paths = cfg.paths;
-
-
 
 // Get Bootstrap Assets
 gulp.task("bs-scss", async () => {
@@ -50,40 +47,32 @@ gulp.task("popper-js", async () => {
 
 // Follow the format above to add new dependencies and add to get-src
 // Get all dependencies to src
-gulp.task("get-src",
-    gulp.series(gulp.series("bs-scss", "bs-js", "popper-js"))
-);
-  
+gulp.task("get-src", gulp.series(gulp.series("bs-scss", "bs-js", "popper-js")));
+
 // Pre-Process
 // Processing Theme Sass
 gulp.task("sass", () => {
   return gulp
-    .src(paths.sass + paths.themeSass)
+    .src(paths.sass + "/**/*.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.write(paths.maps))
     .pipe(gulp.dest(paths.assets + paths.css))
     .pipe(browserSync.stream());
 });
 
-// Process all SaSS with Source Maps
-gulp.task("sass-all-maps", () => {
-    return gulp
-      .src(paths.sass + "/**/*.scss")
-      .pipe(sourcemaps.init())
-      .pipe(sass().on("error", sass.logError))
-      .pipe(sourcemaps.write(paths.maps))
-      .pipe(gulp.dest(paths.assets + paths.css))
-      .pipe(browserSync.stream());
-});
-  
-
 // Minify CSS
 gulp.task("minify-css", () => {
   return gulp
-    .src([paths.assets + paths.css + "/*.css", "!" + paths.assets + paths.css + "/*.min.css"]) // Excludes existing .min.css
+    .src([
+      paths.assets + paths.css + "/*.css",
+      "!" + paths.assets + paths.css + "/*.min.css",
+    ]) // Excludes existing .min.css
     .pipe(
       cleanCSS({ debug: true }, (details) => {
         console.log(`${details.name}: Size: ${details.stats.originalSize}`);
-        console.log(`${details.name}: Minified to" ${details.stats.minifiedSize}`
+        console.log(
+          `${details.name}: Minified to" ${details.stats.minifiedSize}`
         );
       })
     )
@@ -93,29 +82,35 @@ gulp.task("minify-css", () => {
       })
     )
     .pipe(size())
-    .pipe(gulp.dest(paths.assets + paths.css))
+    .pipe(gulp.dest(paths.assets + paths.css));
 });
-
 
 // Cleaning
 // Remove css/custom
 gulp.task("clean-css", () => {
-    return gulp.src(paths.assets + paths.css + paths.custom, { read: false }).pipe(clean());
-  });
+  return gulp
+    .src(paths.assets + paths.css + paths.custom, { read: false })
+    .pipe(clean());
+});
 
 // Delete generated assets
-gulp.task("delete-assets",() => {
-    return gulp.src(paths.assets, { read: false }).pipe(clean());
+gulp.task("delete-assets", () => {
+  return gulp.src(paths.assets, { read: false }).pipe(clean());
 });
 
 // Delete generated CSS
-gulp.task("delete-assets-css",() => {
-    return gulp.src(paths.assets + paths.css, { read: false }).pipe(clean());
+gulp.task("reset-assets-css", () => {
+  return gulp.src(paths.assets + paths.css, { read: false }).pipe(clean());
 });
 
 // Delete generated JS
-gulp.task("delete-assets-js",() => {
-    return gulp.src(paths.assets + paths.js, { read: false }).pipe(clean());
+gulp.task("reset-assets-js", () => {
+  return gulp.src(paths.assets + paths.js, { read: false }).pipe(clean());
+});
+
+// Delete generated JS
+gulp.task("reset-assets-img", () => {
+  return gulp.src(paths.assets + paths.img, { read: false }).pipe(clean());
 });
 
 // Delete Sources
@@ -124,51 +119,89 @@ gulp.task("delete-assets-js",() => {
 // src
 // |-img
 // |-js
-gulp.task("delete-src",() => {
-    return gulp.src(paths.dev, { read: false }).pipe(clean());
+gulp.task("delete-src", () => {
+  return gulp.src(paths.dev, { read: false }).pipe(clean());
+});
+
+// Delete dist
+gulp.task("delete-dist", () => {
+  return gulp.src(paths.dist, { read: false }).pipe(clean());
 });
 
 // Styles [Run sass -> minify-css -> clean-css]
-gulp.task(
-  "styles",
-  gulp.series(gulp.series("sass", "minify-css"))
-);
+gulp.task("styles", gulp.series(gulp.series("sass", "minify-css")));
 
 // Get Scripts
-gulp.task('scripts',async () => {
-    gulp.src(paths.dev + "/**/*.js")
-    .pipe(minify({
+gulp.task("scripts", async () => {
+  gulp
+    .src(paths.dev + "/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write(paths.maps))
+    .pipe(
+      minify({
         ext: {
-            min: '.min.js' // Set the file extension for minified files to .min.js
+          min: ".min.js", // Set the file extension for minified files to .min.js
         },
-        noSource: true // Don’t output a copy of the source file
-    }))
-      .pipe(gulp.dest(paths.assets + paths.js))
+        noSource: true, // Don’t output a copy of the source file
+      })
+    )
+    .pipe(gulp.dest(paths.assets + paths.js));
 });
-
 
 // Image Optimization
-gulp.task('imagemin', async () => {
-    gulp.src(paths.imgsrc)
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 75, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-          plugins: [
-              {removeViewBox: true},
-              {cleanupIDs: false}
-          ]
-      })
-    ]))
-    .pipe(gulp.dest(paths.assets + paths.img))
+gulp.task("imagemin", async () => {
+  gulp
+    .src(paths.imgsrc)
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(gulp.dest(paths.assets + paths.img));
 });
+
+// Build
+gulp.task(
+  "build-assets",
+  gulp.series(gulp.series("sass", "minify-css", "scripts", "imagemin"))
+);
+
+gulp.task(
+  "build-prod",
+  gulp.series(
+    gulp.series("build-assets", async () => {
+      gulp
+        .src([
+          paths.assets + paths.css + paths.themeCss,
+          paths.assets + paths.css + paths.themeCssMin,
+        ])
+        .pipe(gulp.dest(paths.dist + paths.css));
+      gulp
+        .src([
+          paths.assets + paths.js + "/theme.*",
+          paths.assets + paths.js + paths.bootstrap + "/js/bootstrap.min.js",
+          paths.assets + paths.js + paths.popper + "/popper.min.js",
+        ])
+        .pipe(gulp.dest(paths.dist + paths.js));
+      gulp
+        .src([
+          paths.assets + paths.img + "/*"
+        ])
+        .pipe(gulp.dest(paths.dist + paths.img));
+    })
+  )
+);
 
 // Start Server & BrowserSync
 gulp.task(
   "start",
-  gulp.series("styles","scripts","imagemin", function () {
-    browserSync.init( cfg.browserSyncOptions );
+  gulp.series("styles", "scripts", "imagemin", function () {
+    browserSync.init(cfg.browserSyncOptions);
     gulp.watch(paths.sass + "/**/*.scss", gulp.series("styles"));
     gulp.watch(paths.dev + "/**/*.js", gulp.series("scripts"));
     gulp.watch(paths.imgsrc, gulp.series("imagemin"));
